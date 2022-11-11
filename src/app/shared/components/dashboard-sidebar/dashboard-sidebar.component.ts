433db8415f8d2b19fd5e315a27f844791432b7ef
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-11-11 12:32:30 +0300

    Add pure component (onPush)

--- src/app/shared/components/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/shared/components/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-sidebar',
@@ -7,6 +13,10 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardSidebarComponent implements OnInit {
+  //: @Input() ile gelen değerler, parent component tarafından yeniden set edildiğinde/bellek adresi değiştiğinde
+  //: değişiklikleri o an kontrol ediliyor. (3. Yöntem)
+  @Input() dashboardTitle: string = 'Welcome';
+
   menuItems = [
     {
       title: 'Brands',
@@ -28,7 +38,24 @@ export class DashboardSidebarComponent implements OnInit {
     },
   ];
 
-  constructor() {}
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.changeDashboardTitle();
+  }
+
+  changeDashboardTitle(): void {
+    setTimeout(() => {
+      this.dashboardTitle = 'Dashboard';
+      // ...
+      // ...
+      //: OnPush change detection strategy'ini kullanıyorsak, değişiklikleri elle bildirmemiz gerekiyor. (1. Yöntem)
+      this.changeDetectorRef.markForCheck();
+    }, 5000);
+  }
+
+  onMenuItemClick(groupName: string): void {
+    //: HTML tarafında bir event tetiklendiğinde, değişiklikler o an kontrol ediliyor. (2. Yöntem)
+    this.dashboardTitle = groupName;
+  }
 }
--- src/app/shared/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/shared/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -2,13 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './dashboard-layout.component.html',
-  styleUrls: ['./dashboard-layout.component.scss']
+  styleUrls: ['./dashboard-layout.component.scss'],
 })
 export class DashboardLayoutComponent implements OnInit {
+  welcomeText: string = 'Welcome!';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.changeWelcomeText();
   }
 
+  changeWelcomeText() {
+    setTimeout(() => {
+      this.welcomeText = 'Are you afk?';
+    }, 15000);
+  }
 }
